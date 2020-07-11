285f8be598ef9ac2b103d83cf2cc09093d700d15
Miguel Barrios <miguelbarriosmolina@gmail.com>
2020-07-11 13:28:46 +0200

    Maven changes and new empty classes for spring execution.

---
+++ KarumiAppService/src/main/java/com/karumi/AuthenticationApplication.java
@@ -0,0 +1,17 @@
+package com.karumi;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class AuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthenticationApplication.class, args);
+    }
+
+
+}
---
+++ KarumiAppService/src/main/java/com/karumi/AuthenticationController.java
@@ -0,0 +1,10 @@
+package com.karumi;
+
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AuthenticationController {
+
+
+}
